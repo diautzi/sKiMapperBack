fc9121a1533c93f9bd7d6cda55e93916db786e61
Diana Utan <didiutzi@gmail.com>
2020-01-17 13:08:09 -0600

    init

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   namespace :api do
    namespace :v1 do
      resources :trails, :users, :comments, :completed_trails
-
+     get "/", to: "trails"
      post "/login", to: "auth#login"
      get "/current_user", to: "auth#get_user_from_token"
    end
